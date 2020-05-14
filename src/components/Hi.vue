05032ed5d716f232a61ef2c6af462669ed012281
yyp <84702926@qq.com>
2020-05-14 19:26:18 +0800

    vueTest

---
+++ src/components/Hi.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="hi">
+      <h1>{{message}}</h1>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Hi',
+  data () {
+    return {
+      message: 'Hi, I am Vue!'
+    }
+  }
+}
+</script>
+
+<style scoped>
+    .hi{
+      color:green;
+    }
+</style>
